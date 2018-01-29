144d22a219e6e66c314544a243f80011e47be1b5
dynnamitt <kjetil@docstream.no>
2018-01-29 15:27:42 +0100

    should.work

---
+++ redis-cli-inject/inject.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+REDIS_HOST=${REDIS_HOST:-localhost}
+
+echo REDIS_HOST = $REDIS_HOST
+
+ROOTKEY=${1%%=*}
+KV=${1#*=}
+K=${KV%=*}
+V=${KV#*=}
+
+echo "HMSET $ROOTKEY $K $V .."
+
+redis-cli -h $REDIS_HOST HMSET $ROOTKEY $K $V
