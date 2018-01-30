2bdcc88f5ce152b5dfbdca4e5f5dbc2f1b8c2d71
dynnamitt <kjetil@docstream.no>
2018-01-30 13:36:14 +0100

    num.postfix.hack

--- redis-cli-inject/inject.sh
+++ redis-cli-inject/inject.sh
@@ -4,7 +4,9 @@ REDIS_HOST=${REDIS_HOST:-localhost}
 
 echo REDIS_HOST = $REDIS_HOST
 
-ROOTKEY_ENC=${1%%=*}
+# $1 = x_y_z000001=ENV=VAL
+
+ROOTKEY_ENC=$(echo ${1%%=*}| sed 's/[0-9]*$//')
 ROOTKEY_WRKSPC=$(echo $ROOTKEY_ENC|tr _ .)
 KV=${1#*=}
 K=${KV%=*}
